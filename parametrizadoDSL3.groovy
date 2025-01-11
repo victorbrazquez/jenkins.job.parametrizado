f8effb54f3eddb6fc66d0654c18beaf497f81442
victorbrazquez <63924912+victorbrazquez@users.noreply.github.com>
2025-01-11 20:37:14 +0100

    Create parametrizadoDSL3.groovy

---
+++ parametrizadoDSL3.groovy
@@ -0,0 +1,40 @@
+job('ejemplo2-job-DSL') {
+  description('Job DSL de ejemplo para el curso de Jenkins')
+  scm {
+        git('https://github.com/victorbrazquez/jenkins.job.parametrizado.git', 'main') { node ->
+            node / gitConfigName('victorbrazquez')
+            node / gitConfigEmail('vmbv21@gmail.com')
+        }
+    }
+  parameters {
+    stringParam('nombre', defaultValue = 'Julián', description = 'Parámetro de cadena para el Job Booleano')
+    choiceParam('planeta', ['Mercurio', 'Venus', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+    booleanParam('agente', false)
+  }
+  triggers {
+    cron('H/7 * * * *')
+  }
+  steps{
+    shell("bash jobscript.sh")
+  }
+  publishers {
+    mailer('vmbv21@gmail.com', true, true)
+    slackNotifier {
+      notifyAborted(true)
+      notifyEveryFailure(true)
+      notifyNotBuilt(false)
+      notifyUnstable(false)
+      notifyBackToNormal(true)
+      notifySuccess(false)
+      notifyRepeatedFailure(false)
+      startNotification(false)
+      includeTestSummary(false)
+      includeCustomMessage(false)
+      customMessage(null)
+      sendAs(null)
+      commitInfoChoice('NONE')
+      teamDomain(null)
+      authToken(null)
+    }
+  }
+}
